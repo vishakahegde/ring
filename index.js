8be7a6dcd1af5c3d916e4d71f68d9427962f42de
vishakahegde <hegde.vishaka@gmail.com>
2020-04-23 11:38:15 +0200

    Exercise to add ring folder to git

---
+++ index.js
@@ -0,0 +1,9 @@
+const toTitleCase = require ('to-title-case');
+
+// console.log(toTitleCase);
+
+const lower = 'the lord of the rings: the return of the king';
+
+const title = toTitleCase(lower);
+
+console.log(title);
